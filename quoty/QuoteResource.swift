d91b4786c7d02abc18a308c3197647e65cca8c01
yOoMarvin <marvin.messenzehl@gmail.com>
2017-03-16 09:17:15 +0100

    Added quote resource for downloading the data

---
+++ quoty/QuoteResource.swift
@@ -0,0 +1,28 @@
+//
+//  QuoteResource.swift
+//  quoty
+//
+//  Created by Marvin Messenzehl on 16.03.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+class QuoteResource {
+    
+    let urlText: String = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?"
+    
+    
+    // MARK: Download data
+    
+    //download
+    //async
+    
+    
+    
+    // MARK: Response
+    
+    //create response
+    //read out the JSON
+    //maybe help function for parsing the json
+}
