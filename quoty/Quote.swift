1c235a110a58d402d66a65badef12643c313742d
yOoMarvin <marvin.messenzehl@gmail.com>
2017-03-16 09:04:40 +0100

    Added quote data model

---
+++ quoty/Quote.swift
@@ -0,0 +1,16 @@
+//
+//  Quote.swift
+//  quoty
+//
+//  Created by Marvin Messenzehl on 16.03.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+struct quote {
+    
+    let author: String
+    let text: String
+    
+}
